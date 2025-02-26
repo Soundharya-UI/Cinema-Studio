e332bda3f1b5d71dfd7e8616a02dc73c22eadcf2
soundharya-UI <soundharya.s2611@gmail.com>
2025-02-26 12:43:15 +0530

    footer changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,9 +6,9 @@ export const Footer = () => {
       <footer className="bg-white shadow-sm dark:bg-gray-800">
         <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
           <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-            © 2023{" "}
+            © 2025{" "}
             <Link to="/" className="hover:underline">
-              my app
+              Cinema Studio
             </Link>
             . All Rights Reserved.
           </span>
